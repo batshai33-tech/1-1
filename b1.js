cf55df94050da8a12a1fe129a28004d87aa59435
batshai33-tech <bat.shai33@gmail.com>
2026-03-08 23:33:00 -0700

    asd
    asd

---
+++ b1.js
@@ -0,0 +1,14 @@
+<script>
+const a = 10
+console.log(a);
+</script>
+// javascript
+// 브라우저
+// 터미널
+//cmd z 뒤로
+//cmd 쉬프트 z 앞으로 가기
+// option 쉬프트 f
+// cmd s 저장
+// cmd r 새로고침
+// cmd -> 소스 제일 뒤로
+// cmd <- 소스 제일 왼쪽으로
